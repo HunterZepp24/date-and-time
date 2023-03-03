28dd1bcc9e372bf947992749c0e1f8897326d31e
Hunter Zepp <122405685+HunterZepp24@users.noreply.github.com>
2023-03-02 22:01:04 -0800

    Updated HTML, CSS, JS and Fixed Fonts

---
+++ js/script.js
@@ -0,0 +1,45 @@
+gsap.from(".seconds", {
+    opacity: 0,
+    duration: 0.25,
+    repeat: -1,
+    repeatDelay: 0.75,
+    ease: "power1.inOut"
+})
+gsap.to(".seconds", {
+    opacity: 0,
+    duration: 0.25,
+    delay: 0.75,
+    repeat: -1,
+    repeatDelay: 0.75,
+    ease: "power1.inOut"
+})
+gsap.from(".minutes", {
+    opacity: 0,
+    duration: 0.25,
+    repeat: -1,
+    repeatDelay: 0.75,
+    ease: "power1.inOut"
+})
+gsap.to(".minutes", {
+    opacity: 0,
+    duration: 0.25,
+    delay: 0.75,
+    repeat: -1,
+    repeatDelay: 0.75,
+    ease: "power1.inOut"
+})
+gsap.from(".hours", {
+    opacity: 0,
+    duration: 0.25,
+    repeat: -1,
+    repeatDelay: 0.75,
+    ease: "power1.inOut"
+})
+gsap.to(".hours", {
+    opacity: 0,
+    duration: 0.25,
+    delay: 0.75,
+    repeat: -1,
+    repeatDelay: 0.75,
+    ease: "power1.inOut"
+})

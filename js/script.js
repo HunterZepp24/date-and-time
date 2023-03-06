7fd4526b6826b4cd4ef4974407982ec010439c0f
Hunter Zepp <122405685+HunterZepp24@users.noreply.github.com>
2023-03-05 19:10:08 -0800

    Adding Styles and Animations

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+//Working on getting hours, minutes, and seconds to flash separately versus every second.
+
+/*const timeNow = new Date();
+
+let gotHours = timeNow.getHours();
+let gotMinutes = timeNow.getMinutes();
+let gotSeconds = timeNow.getSeconds();
+
+const hours = document.querySelector(".hours");
+const minutes = document.querySelector(".minutes");
+const seconds = document.querySelector(".seconds");*/
+
 gsap.from(".seconds", {
     opacity: 0,
     duration: 0.25,
@@ -43,3 +55,11 @@ gsap.to(".hours", {
     repeatDelay: 0.75,
     ease: "power1.inOut"
 })
+
+gsap.to("body", {
+    backgroundColor: "#0096FF",
+    duration: 3,
+    repeat: -1,
+    yoyo: true,
+    ease: "power1.inOut"
+  })
